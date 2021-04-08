e3d0736173216ea33c652a7dd847931921aa744a
Malcolm Reyes <mreyesx99@gmail.com>
2021-04-08 18:01:54 -0400

    add question routes

---
+++ backend/routes/api/question.js
@@ -0,0 +1,39 @@
+const { response } = require('express');
+const express = require('express');
+const router = express.Router();
+
+const Question = require('../../models/question')
+
+router.get('/', (req, res) => {
+    Question.find()
+        .then(questions => res.json(questions))
+            .catch(err => res.status(404).json(err))
+})
+
+router.post('/',  (req, res) => {
+    const newQuestion = new Question({
+        name: req.body.name,
+        text: req.body.text,
+        repo: req.body.repo,
+        live: req.body.live
+    });
+
+    newQuestion.save()
+        .then(question => res.json(question))
+            .catch(err => res.status(404).json(err))
+})
+
+router.patch('/:question_id', (req, res) => {
+    const Question = Question.findOneAndUpdate({_id: question._id},{$set: req.body},{new: true, useFindAndModify: false})
+        .then(question => res.json(question))
+            .catch(err => res.status(404).json(err))
+    
+})
+
+router.delete('/:question_id', (req, res) => {
+    Question.findOneAndDelete({_id: req.params.question_id})
+        .then(question => res.json({_id: question._id}))
+            .catch(err => res.status(404).json(err))
+})
+
+module.exports = router
