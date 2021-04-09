ee33051ba86163c9dc2b3fcb6a67d946d19d70e1
Malcolm Reyes <mreyesx99@gmail.com>
2021-04-09 14:25:37 -0400

    add question actions

---
+++ frontend/src/actions/question_actions.js
@@ -0,0 +1,56 @@
+import {createAction} from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const recieveQuestions = createAction('RECIEVE_QUESTIONS')
+export const recieveQuestion = createAction('RECIEVE_QUESTION')
+export const removeQuestion = createAction('REMOVE_QUESTION')
+
+export const fetchQuestions = () => async dispatch => {
+    try{
+        const res = await axios.get('http://localhost:5000/api/questions')
+        dispatch(recieveQuestions(res.data))
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+export const submitQuestion = question => async dispatch => {
+    try{
+        const res = await axios.post('http://localhost:5000/api/questions', question)
+        dispatch(recieveQuestion(res.data))
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+export const updateQuestion = question => async dispatch => {
+    try{
+        const res = await axios.patch('http://localhost:5000/api/questions', question)
+        dispatch(recieveQuestion(res.data))
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+export const fetchQuestion = id => async dispatch => {
+    try{
+        const res = await axios.get(`http://localhost:5000/api/questions/${id}`)
+        dispatch(recieveQuestion(res.data))
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+export const deleteQuestion = id => async dispatch => {
+    try{
+        const res = await axios.delete(`http://localhost:5000/api/questions/${id}`)
+        dispatch(removeQuestion(res.data))
+    }
+    catch(err){
+        console.log(err)
+    }
+}
